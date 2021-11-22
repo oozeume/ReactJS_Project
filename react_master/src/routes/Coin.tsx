e0a82d65df9193c308df80b1eac555f2a067cc8d
Woo Jieum <oozeume@gmail.com>
2021-11-23 00:19:37 +0900

    chore: Route Setup

---
+++ react_master/src/routes/Coin.tsx
@@ -0,0 +1,18 @@
+import { useParams } from 'react-router';
+
+interface RouteParams {
+  coinId: string;
+}
+
+function Coin() {
+  const params = useParams() as RouteParams;
+  const coinId = params.coinId;
+
+  return (
+    <>
+      <h1>Coin: {coinId}</h1>
+    </>
+  );
+}
+
+export default Coin;
