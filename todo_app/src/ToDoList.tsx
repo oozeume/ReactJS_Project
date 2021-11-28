53b9d0c87071dff90795c2c595b13ee67e495708
Woo Jieum <oozeume@gmail.com>
2021-11-28 17:22:48 +0900

    feature: form완료(custom validation)

--- todo_app/src/ToDoList.tsx
+++ todo_app/src/ToDoList.tsx
@@ -1,69 +1,33 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { getNodeMajorVersion } from 'typescript';
 
 interface IForm {
-  Email: string;
-  FirstName: string;
-  LastName: string;
-  Password: string;
-  PasswordComfirm: string;
+  Todo: string;
+  ExtraError?: string; // 필수가 아닐 때 작성하는 방법
 }
 
 function ToDoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IForm>({
+  const { register, handleSubmit, setValue } = useForm<IForm>({
     defaultValues: {
       // 기본값 설정
-      Email: '@naver.com',
     },
   });
-  // 모든 validation을 다 마쳤을 때 호출
-  const onValid = (data: any) => {};
+
+  // 유효하다면 실행할 함수
+  const handleValid = (data: IForm) => {
+    setValue('Todo', '');
+  };
+
   return (
     <>
       <div>
-        <form onSubmit={handleSubmit(onValid)}>
+        <form onSubmit={handleSubmit(handleValid)}>
           <input
-            {...register('Email', {
-              required: 'Email is required',
-              pattern: {
-                value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-                message: 'Only naver.com emails allowed',
-              },
+            {...register('Todo', {
+              required: 'please write todo',
             })}
-            placeholder="Email"
+            placeholder="Write a to do"
           />
-          <span>{errors?.Email?.message}</span>
-          <input
-            {...register('FirstName', { required: 'Write here' })}
-            placeholder="First Name"
-          />
-          <span>{errors?.FirstName?.message}</span>
-          <input
-            {...register('LastName', { required: 'Write here' })}
-            placeholder="Last Name"
-          />
-          <span>{errors?.LastName?.message}</span>
-          <input
-            {...register('Password', {
-              required: 'Write here',
-              minLength: {
-                value: 5,
-                message: 'Your password is too short',
-              },
-            })}
-            placeholder="Password"
-          />
-          <span>{errors?.Password?.message}</span>
-          <input
-            {...register('PasswordComfirm', { required: 'Write here' })}
-            placeholder="Password Comfirm"
-          />
-          <span>{errors?.PasswordComfirm?.message}</span>
           <button>Add</button>
         </form>
       </div>
