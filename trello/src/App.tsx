5b322c0c17caf6a85fb65a045e63c74c5c8e69b3
Woo Jieum <oozeume@gmail.com>
2021-12-01 15:20:54 +0900

    Setup

---
+++ trello/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from 'recoil';
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ trello/src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  bgColor: '#2d3436',
+  cardbgColor: 'whitesmoke',
+  textColor: 'black',
+  accentColor: '#ff7675',
+};
