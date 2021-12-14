91acd94799466e284e1a81680774c1eb835966d3
Woo Jieum <oozeume@gmail.com>
2021-12-14 23:40:54 +0900

    진행중

--- todo_list/src/components/CreateToDo.tsx
+++ todo_list/src/components/CreateToDo.tsx
@@ -1,12 +1,18 @@
 import {useForm } from 'react-hook-form';
 
-function CreateToDo() {
-  const {register} = useForm();
-
+interface FormData {
   
+}
+
+function CreateToDo() {
+  const {register} = useForm<FormData>();
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
     <>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
         <input type="text" placeholder="write todo" {
           ...register('Todo', {required: 'please write todo'})
         } />
